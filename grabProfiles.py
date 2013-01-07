26e0f2518879523fd218f75d046e5d1632b1a34d
Moony Chou <mengyu.chou@gmail.com>
2013-01-07 21:48:32 +0800

    basic grabber of profiles
    still need code to meet the limit of frequency, and parsing the grabbed
    html pages

--- grabProfiles.py
+++ grabProfiles.py
@@ -1,12 +1,21 @@
 from rrBrowser import RenrenBrowser
+from rrParser import RenrenParser
 from rrRecorder import RenrenRecorder
 
 
 storePath = 'D:/Projects/NetSci/U&I/data'
-mergedRec = RenrenRecorder(path=storePath)
-print(len(mergedRec.getProfileList()))
 
 rrID = input("Your Renren ID (e.g.239486743): ")
 rrUser = input("Your Renren Login Email: ")
 rrPassword = input("Your Renren Password: ")
+
+browser = RenrenBrowser(user=rrUser, passwd=rrPassword, path=storePath)
+browser.setLogLevel(40)
+browser.login()
+mergedRec = RenrenRecorder(path=storePath, writeBack=True)
+parser = RenrenParser(browser, mergedRec)
+
+#TODO: add frequency limit
+for rrID in mergedRec.getProfileList().keys():
+    browser.grabProfilePage(rrID)
 
